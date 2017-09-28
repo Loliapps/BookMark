7053c556c91d898227a5dd883876c0c71caa2fe1
Loliapps <lilachberkovich@gmail.com>
2017-09-28 09:16:01 +0300

    Add files via upload

---
+++ BookMark/app/src/main/java/com/myapplication/listeners/DataReceivedListener.java
@@ -0,0 +1,10 @@
+package com.myapplication.listeners;
+
+/**
+ * Created by home on 6/28/2017.
+ */
+
+public interface DataReceivedListener {
+
+    public void onDataReceived(String data);
+}
---
+++ BookMark/app/src/main/java/com/myapplication/listeners/UserImageDownloadListener.java
@@ -0,0 +1,12 @@
+package com.myapplication.listeners;
+
+import java.io.File;
+
+/**
+ * Created by home on 11/21/2016.
+ */
+
+public interface UserImageDownloadListener {
+
+    public void onUserImageDownloadCompleted(String FileName);
+}
